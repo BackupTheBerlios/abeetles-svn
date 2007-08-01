14565309be00a9a53949701f52345b43d5fb2795
ibart <ibart@60a5a0de-1a2f-0410-942a-f28f22aea592>
2007-08-01 15:47:59 +0000

    Docistena kapitola 4

--- trunk/gui/abeetlesgui/RunScript.cpp
+++ trunk/gui/abeetlesgui/RunScript.cpp
@@ -18,6 +18,7 @@ CRunScript::CRunScript(QString script)//QList<COneRun*> listRuns)
 int CRunScript::run()
 {
 	COneRun * oneRun;
+	CEnvironment * env;
 	int l;
 	for (l=0;l<ListRuns.size();l++)
 	{
@@ -50,7 +51,7 @@ int CRunScript::run()
 
 		oneRun=	ListRuns.at(l);
 		//QMessageBox::information (NULL,"","1");
-		CEnvironment env (oneRun);
+		env = new CEnvironment(oneRun);
 		//QMessageBox::information (NULL,"","2");
 		QDir::setCurrent(oneRun->DirName);
 		int time;
@@ -59,54 +60,54 @@ int CRunScript::run()
 		//QMessageBox::information (NULL,"","Life "+oneRun->DirName+" starts");
 		for(time=0;time<=oneRun->EndTime;time++)
 		{
-			for(I=0;I<env.Grid_Past.G_Width;I++)
-			for(J=0;J<env.Grid_Past.G_Height;J++)
+			for(I=0;I<env->Grid_Past.G_Width;I++)
+			for(J=0;J<env->Grid_Past.G_Height;J++)
 			{
-				if (env.Grid_Past.GetCellContent(I,J)==BEETLE) env.MakeBeetleAction(I,J);
-				else if (env.Grid_Past.GetCellContent(I,J)==NOTHING) env.MakeFlowerGrow(I,J);
-				else if (env.Grid_Past.GetCellContent(I,J)==FLOWER) env.MakeFlowerDie(I,J);
+				if (env->Grid_Past.GetCellContent(I,J)==BEETLE) env->MakeBeetleAction(I,J);
+				else if (env->Grid_Past.GetCellContent(I,J)==NOTHING) env->MakeFlowerGrow(I,J);
+				else if (env->Grid_Past.GetCellContent(I,J)==FLOWER) env->MakeFlowerDie(I,J);
 				//if there is a wall, flower of something bad, do nothing
 			}
 
 
 			
-			//if (time%10==0) env.Statist.SaveActAgrStatist(oneRun->AggrStatFN.toAscii().data(),time);
+			//if (time%10==0) env->Statist.SaveActAgrStatist(oneRun->AggrStatFN.toAscii().data(),time);
 			if (time==oneRun->SaveTimesAggr[i_agr])
 			{
-				env.CountStatistics();
+				env->CountStatistics();
 				i_agr++;
-				env.Statist.SaveActAgrStatist((QString::number(time)+oneRun->AggrStatFN).toAscii().data(),time);
+				env->Statist.SaveActAgrStatist((QString::number(time)+oneRun->AggrStatFN).toAscii().data(),time);
 			}
 			if (time==oneRun->SaveTimesHist[i_hist])
 			{
 				i_hist++;
-				env.Statist.SaveActHistStatist((QString::number(time)+oneRun->HistStatFN).toAscii().data(),time,&(env.Grid));
+				env->Statist.SaveActHistStatist((QString::number(time)+oneRun->HistStatFN).toAscii().data(),time,&(env->Grid));
 			}
 			if (time==oneRun->SaveTimesEnv[i_env])
 			{
 				i_env++;
-				env.SaveEnv((oneRun->EnvFN+QString::number(time)).toAscii().data());
+				env->SaveEnv((oneRun->EnvFN+QString::number(time)).toAscii().data());
 			}
 			if ((oneRun->SaveTimeAggrReg!=0) &&(time % oneRun->SaveTimeAggrReg==0))		
 			{
-				env.CountStatistics();
-				env.Statist.SaveActAgrStatist((QString::number(time)+oneRun->AggrStatFN).toAscii().data(),time);
+				env->CountStatistics();
+				env->Statist.SaveActAgrStatist((QString::number(time)+oneRun->AggrStatFN).toAscii().data(),time);
 			}
 
 			if ((oneRun->SaveTimeHistReg!=0) &&(time % oneRun->SaveTimeHistReg==0))		
-				env.Statist.SaveActHistStatist((QString::number(time)+oneRun->HistStatFN).toAscii().data(),time,&(env.Grid));
+				env->Statist.SaveActHistStatist((QString::number(time)+oneRun->HistStatFN).toAscii().data(),time,&(env->Grid));
 
 			if ((oneRun->SaveTimeEnvReg!=0) &&(time % oneRun->SaveTimeEnvReg==0))	
-				env.SaveEnv((oneRun->EnvFN+QString::number(time)).toAscii().data());
+				env->SaveEnv((oneRun->EnvFN+QString::number(time)).toAscii().data());
 				
 
-			env.NextTime();
+			env->NextTime();
 
 		}
-		env.Statist.SaveTimeStatist_InColumnsAppend(oneRun->TimeStatFN.toAscii().data());
+		env->Statist.SaveTimeStatist_InColumnsAppend(oneRun->TimeStatFN.toAscii().data());
 		QDir::setCurrent("..");
 		fprintf(stdout,("\nScript "+oneRun->DirName+" finnished.\n\n").toAscii().data());
-
+		delete(env);
 	}
 	return 1;
 }
